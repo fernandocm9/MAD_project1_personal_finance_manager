7357552c05240a78bb933da22945556f42a53668
Fernando Curiel <fernando@Abigails-Laptop.local>
2025-03-18 12:34:53 -0400

    adjusted ndk version and set up columns for finance logs

--- finance_manager/lib/finance_db.dart
+++ finance_manager/lib/finance_db.dart
@@ -3,12 +3,13 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DatabaseHelper {
-  static const _databaseName = "MyDatabase.db";
+  static const _databaseName = "finance.db";
   static const _databaseVersion = 1;
-  static const table = 'my_table';
+  static const table = 'logs';
   static const columnId = '_id';
   static const columnName = 'name';
-  static const columnAge = 'age';
+  static const columnCategory = 'category';
+  static const columnAmount = 'amount';
   late Database _db;
 // this opens the database (and creates it if it doesn't exist)
   Future<void> init() async {
@@ -27,7 +28,8 @@ class DatabaseHelper {
   CREATE TABLE $table (
   $columnId INTEGER PRIMARY KEY,
   $columnName TEXT NOT NULL,
-  $columnAge INTEGER NOT NULL
+  $columnCategory TEXT NOT NULL,
+  $columnAmount INTEGER NOT NULL
 )
 ''');
   }
