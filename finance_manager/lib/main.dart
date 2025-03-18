3192352e687f3858b47d400e4c3385efb35bf97c
Fernando Curiel <fernando@Abigails-Laptop.local>
2025-03-18 13:03:49 -0400

    created tabs for logs and graphs

--- finance_manager/lib/main.dart
+++ finance_manager/lib/main.dart
@@ -10,12 +10,36 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+    //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    //   ),
+    //   home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    // );
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DefaultTabController(length: 2, child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('CashFlow')),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Tab 1'),
+              Tab(text: 'Tab 2'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            // Center(child: Text('Tab 1 Content')),
+            MyHomePage(title: 'sdf'),
+            Center(child: Text('Tab 2 Content')),
+          ],
+        ),
+      )),
     );
   }
 }
@@ -46,6 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -61,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
